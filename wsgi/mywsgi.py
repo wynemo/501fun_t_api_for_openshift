893be32ba5eb8666bd7a0b4f04d7cfc0fa860c58
zdb <zdb@kubuntu.(none)>
2012-04-24 02:04:54 +0800

    change layout

---
+++ wsgi/mywsgi.py
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env python
+#coding:utf-8
+
+import web
+from config import settings
+
+urls = ('/','twitterapi.ttcbk.index',
+    '/auth','twitterapi.ttauth.auth',
+    '/api/(.*)','twitterapi.ttapi.api',
+)
+
+app = web.application(urls, globals())
+application = app.wsgifunc()
+if __name__=="__main__":
+#    app.run()
+    pass
+
